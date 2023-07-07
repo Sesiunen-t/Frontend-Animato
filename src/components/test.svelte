99f9e8095de4c83f15d5e905df498613c36ce3e0
Erupturatis <barbulescu.eugeno@gmail.com>
2023-07-07 22:23:05 +0300

    (create)[Fri Jul  7 10:23:05 PM EEST 2023] boilerplate

---
+++ src/components/test.svelte
@@ -0,0 +1,20 @@
+<script>
+    let name = 'World';
+</script>
+
+<main>
+    <h1 class="w-20 border-2 border-black h-20 ">Hello {name}!</h1>
+</main>
+
+
+<style>
+    main {
+        text-align: center;
+        padding: 1rem;
+        font-family: sans-serif;
+    }
+
+    h1 {
+        color: #333;
+    }
+</style>
